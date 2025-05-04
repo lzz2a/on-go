aef66df4f4c259d7c8b4630c35f0fccf57672815
lzz2a <141039823+lzz2a@users.noreply.github.com>
2025-05-04 02:56:13 -0400

    Add files via upload

--- tobiware.lua
+++ tobiware.lua
@@ -1,4 +1,6 @@
 local repo = "https://raw.githubusercontent.com/lzz2a/on-go/refs/heads/main/tobiware.lua";
+local ui = repo .. "/ui";
+local features = repo .. "/features";
 
 local function check(path)
     if not isfolder(path) then
@@ -7,5 +9,25 @@ local function check(path)
 end
 
 check("tobiware")
+check("tobiware/ui");
 check("tobiware/features");
+
+local ui_files = {"constructor.lua","handler.lua"}
+local features_files = {"mags.lua", "dynamic_jump.lua"};
+
+for i,v in pairs(ui_files) do
+    local path = `tobiware/ui/{v}`
+    writefile(path, game:HttpGet(`{ui}/{v}`))
+end 
+
+for i,v in pairs(features_files) do
+    local path = `tobiware/features/{v}`
+    writefile(path, game:HttpGet(`{features}/{v}`))
+end 
+
+getgenv().include = function(path)
+    return loadfile(`{path}.lua`);
+end
+
+local ui = include("ui/constructor");
 
