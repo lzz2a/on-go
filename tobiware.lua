c154b1c8f93667ce8da458018fa0f162d25f3c8f
lzz2a <141039823+lzz2a@users.noreply.github.com>
2025-05-04 02:55:52 -0400

    Add files via upload

--- tobiware.lua
+++ tobiware.lua
@@ -1,1 +1,11 @@
+local repo = "https://raw.githubusercontent.com/lzz2a/on-go/refs/heads/main/tobiware.lua";
+
+local function check(path)
+    if not isfolder(path) then
+        makefolder(path);
+    end
+end
+
+check("tobiware")
+check("tobiware/features");
 
